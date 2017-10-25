6fba6d5391b31f749d48377edd78a67c41c3fd78
Casper <casper.boon@uwa.edu.au>
2017-10-25 11:39:39 +0800

    added support for travis

--- build_tfv_aed.sh
+++ build_tfv_aed.sh
@@ -4,35 +4,6 @@ if [ "$FV_CONFIGURED" != "true" ] ; then
   . ./FV_CONFIG
 fi
 
-if [ "$FORTRAN_COMPILER" = "IFORT" ] ; then
-   if [ -d /opt/intel/bin ] ; then
-      . /opt/intel/bin/compilervars.sh intel64
-   fi
-   which ifort >& /dev/null
-   if [ $? != 0 ] ; then
-      echo ifort compiler requested, but not found
-      exit 1
-   fi
-fi
-
-if [ "$FORTRAN_COMPILER" = "IFORT" ] ; then
-   export PATH="/opt/intel/bin:$PATH"
-   export FC=ifort
-   export NETCDFHOME=/opt/intel
-else
-   export FC=gfortran
-   export NETCDFHOME=/usr
-   if [ "$OSTYPE" == "Darwin" ] ; then
-     export NETCDFHOME=/opt/local
-   fi
-fi
-
-export F77=$FC
-export F90=$FC
-export F95=$FC
-
-export MPI=OPENMPI
-
 if [ "$AED2DIR" = "" ] ; then
   export AED2DIR=../libaed2
 fi
