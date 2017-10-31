2d0f4355bf9a4890459286da60bc0fd61d3ca831
Casper <casper.boon@uwa.edu.au>
2017-10-31 14:11:37 +0800

    travis test

--- build_tfv_aed.sh
+++ build_tfv_aed.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ "$FV_CONFIGURED" != "true" ] ; then
   . ./FV_CONFIG
---
+++ clean_tfv_aed.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$FV_CONFIGURED" != "true" ] ; then
+  . ./FV_CONFIG
+fi
+
+if [ "$AED2DIR" = "" ] ; then
+  export AED2DIR=../libaed2
+fi
+
+cd ${AED2DIR}
+make distclean
+cd ${CURDIR}
+
+if [ -d ${AED2PLS} ] ; then
+   cd ${AED2PLS}
+   make distclean
+   cd ${CURDIR}
+fi
+
+make distclean
